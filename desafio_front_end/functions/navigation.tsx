ab6ad4165a0ef42098b71df14097f0eac4679974
denner augusto <denner.augusto90@gmail.com>
2023-02-23 17:57:10 -0300

    atualizando links da navegação

--- desafio_front_end/functions/navigation.tsx
+++ desafio_front_end/functions/navigation.tsx
@@ -5,10 +5,8 @@ export function Navigation() {
     const { setLinkActiveNow } = useContext(Context)
     const linkNavigation = ["Home", "Pessoa Usuária", "Profissional"]
 
-
-
     return linkNavigation.map((links, index) => {
-        return <a onClick={() => setLinkActiveNow(index + 1)} key={links}>{links}</a>
+        return <a onClick={() => setLinkActiveNow(index + 1)} key={Math.random()}>{links}</a>
     })
 
 }
