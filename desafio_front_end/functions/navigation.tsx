7e3a2c2d41e5580e24e87c3c4e6a36a8d99de8ab
denner augusto <denner.augusto90@gmail.com>
2023-02-23 15:17:13 -0300

    feat(component): criando component responsavel pelos links

---
+++ desafio_front_end/functions/navigation.tsx
@@ -0,0 +1,14 @@
+import { Context } from "context/context";
+import { useContext } from "react";
+
+export function Navigation() {
+    const { setLinkActiveNow } = useContext(Context)
+    const linkNavigation = ["Home", "Pessoa Usuária", "Profissional"]
+
+
+
+    return linkNavigation.map((links, index) => {
+        return <a onClick={() => setLinkActiveNow(index + 1)} key={links}>{links}</a>
+    })
+
+}
