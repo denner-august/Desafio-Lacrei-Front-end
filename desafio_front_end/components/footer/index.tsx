515510cd944d0672b2e1eb2e053e0084664a0dcc
denner augusto <denner.augusto90@gmail.com>
2023-02-23 17:56:41 -0300

    atualizando footer

--- desafio_front_end/components/footer/index.tsx
+++ desafio_front_end/components/footer/index.tsx
@@ -1,19 +1,36 @@
-import { Navigation } from "functions/navigation";
-import { Container } from "./footerStyles";
 import { useContext } from "react";
 import { Context } from "context/context";
+import { Navigation } from "functions/navigation";
+import { Container } from "./footerStyles";
+
+import instagram from '../../public/imagens/InstagramLogo.png'
+import facebook from '../../public/imagens/FacebookLogo.png'
+import linkedin from '../../public/imagens/LinkedinLogo.png'
+import Image from "next/image";
 
 export function Footer() {
 
     const { linkActiveNow } = useContext(Context)
 
+    function renderIcons() {
+        const icons = [facebook, instagram, linkedin]
+
+        return icons.map((item, index) => {
+            return <Image className="Social" src={item} width={32} height={32} alt="teste" key={index} />
+        })
+
+    }
+
     return (
         <Container linkActive={linkActiveNow}>
-            <ul>
+            <ul className="links">
                 {Navigation()}
             </ul>
 
-            <h1>imagem</h1>
+            <ul className="ContainerSocial">
+                {renderIcons()}
+            </ul>
+
 
             <p>Desafio Front-end Lacrei</p>
         </Container>
