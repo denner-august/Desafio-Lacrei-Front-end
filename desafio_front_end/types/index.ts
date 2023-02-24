b11f46821369ab9357be894ce4a36b9e63fd6ce7
denner augusto <denner.augusto90@gmail.com>
2023-02-24 20:22:36 -0300

    atualizando types

--- desafio_front_end/types/index.ts
+++ desafio_front_end/types/index.ts
@@ -1,4 +1,10 @@
 export interface ContextProviderProps {
   linkActiveNow: number;
   setLinkActiveNow: (value: number) => void;
+  rotas: (index: number) => void;
+}
+export interface main_descricao_Props {
+  titulo: string;
+  descricao: string;
+  buttons: boolean;
 }
