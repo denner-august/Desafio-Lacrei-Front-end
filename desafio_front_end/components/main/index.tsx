adb898b734fc91c709e0c34e8e00db64f1523c04
denner augusto <denner.augusto90@gmail.com>
2023-02-24 20:25:39 -0300

    atualizando component

--- desafio_front_end/components/main/index.tsx
+++ desafio_front_end/components/main/index.tsx
@@ -1,11 +1,12 @@
+import { main_descricao_Props } from "types"
 import { Descricao } from "./descricao"
 import { RenderImage } from "./renderImage"
 import { Container } from "./styles/MainStyles"
 
-export function Main() {
+export function Main({ titulo, descricao, buttons = true }: main_descricao_Props) {
     return (
         <Container>
-            <Descricao titulo="Boas vindas a Lacrei Saúde" descricao="Uma plataforma segura e acolhedora para comunidade LGBTQIAPN+" buttons={true} />
+            <Descricao titulo={titulo} descricao={descricao} buttons={buttons} />
             <RenderImage />
         </Container>
     )
