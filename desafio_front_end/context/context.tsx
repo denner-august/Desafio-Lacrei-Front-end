81a88da584775d2fecab9d86349b48cdec62c62c
denner augusto <denner.augusto90@gmail.com>
2023-02-24 20:25:05 -0300

    feat(context): adicionando funcao que controla envio de rota e link ativo no momento

--- desafio_front_end/context/context.tsx
+++ desafio_front_end/context/context.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useState } from "react";
 import { ContextProviderProps } from "types";
+import { useRouter } from 'next/router'
 
 interface contextProps {
     children: ReactNode
@@ -8,11 +9,33 @@ interface contextProps {
 export const Context = createContext({} as ContextProviderProps)
 
 export function ContextProvider({ children }: contextProps) {
+    const router = useRouter()
 
     const [linkActiveNow, setLinkActiveNow] = useState(1)
 
+    function rotas(index: number) {
+        switch (index) {
+            case 0:
+                router.push("/")
+                setLinkActiveNow(index + 1)
+                break;
+            case 1:
+                router.push("pessoa-usuaria")
+                setLinkActiveNow(index + 1)
+                break;
+
+            case 2:
+                router.push("profissional")
+                setLinkActiveNow(index + 1)
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return (
-        <Context.Provider value={{ linkActiveNow, setLinkActiveNow }}>
+        <Context.Provider value={{ linkActiveNow, setLinkActiveNow, rotas }}>
             {children}
         </Context.Provider>
     )
