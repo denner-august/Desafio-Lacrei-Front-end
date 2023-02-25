5a7a98c01b152f9a52a955c6d97264c1990ef080
denner augusto <denner.augusto90@gmail.com>
2023-02-25 14:59:48 -0300

    atualizando nextConfig

--- desafio_front_end/next.config.js
+++ desafio_front_end/next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
   },
   images: {
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   }
 }
