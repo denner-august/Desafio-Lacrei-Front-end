7dedc95af5efe898decfbb7640b32cb1d9895d95
denner augusto <denner.augusto90@gmail.com>
2023-02-25 14:48:03 -0300

    objeto responsavel pelas imagens

---
+++ desafio_front_end/context/imagens.ts
@@ -0,0 +1,15 @@
+import imagem1 from "../public/imagens/rafiki 2.svg";
+import imagem2 from "../public/imagens/rafiki3.svg";
+import imagem3 from "../public/imagens/profissional 1.svg";
+
+type imagens = {
+  imagem1: string;
+  imagem2: string;
+  imagem3: string;
+};
+
+export const imagens: imagens = {
+  imagem1,
+  imagem2,
+  imagem3,
+};
