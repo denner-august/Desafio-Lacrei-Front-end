2d648ab222e88cce8584d169235cddc2c14199ea
denner augusto <denner.augusto90@gmail.com>
2023-02-25 14:48:24 -0300

    atualizando font

--- desafio_front_end/components/main/descricao.tsx
+++ desafio_front_end/components/main/descricao.tsx
@@ -10,7 +10,7 @@ export function Descricao({ titulo, descricao, buttons }: main_descricao_Props) 
     return (
         <Container className="Descricao">
             <h1 className={fontNunito700.className}>{titulo}</h1>
-            <p className={fontNunito400.className}>{descricao}</p>
+            <p className={fontNunito700.className}>{descricao}</p>
             {buttons === true ? <div>
                 <button onClick={() => rotas(1)}>Pessoa Usuária</button>
                 <button onClick={() => rotas(2)}>Profissional</button>
